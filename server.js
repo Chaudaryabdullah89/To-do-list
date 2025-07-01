fcc41d1414b94a692a1afdf321b7b702949f0d8a
Chaudaryabdullah89 <chaudaryabdullah899@gmail.com>
2025-07-01 13:39:28 +0500

    d12

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set('view engine', 'ejs');
+app.set('views', __dirname + '/Views');
 
 app.get('/', isAuthenticated, async (req, res) => {
     try {
